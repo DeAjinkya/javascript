db021896abb8c97b6c02f445b101c4b6f1b9f951
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-09 23:51:35 +0530

    map() method

---
+++ map-method/map.js
@@ -0,0 +1,10 @@
+
+/**
+ * The map() method is used for creating a new array from the existing one by applying
+ * a function to each one of the elements of the first array.
+ */
+
+let nums = [3,8,12,16];
+let multy = nums.map((num) => num * num);
+
+console.log(multy); //output: [9,64,144,256]
