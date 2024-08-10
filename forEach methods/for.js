a11b7af600768ca58de8aff15b0ed1980b2995cd
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-10 23:30:03 +0530

    forEach() method

---
+++ forEach methods/for.js
@@ -0,0 +1,7 @@
+// The forEach() method receives a function as an 
+// argument and executes it once for each array element.
+
+let nums = [1,2,3,4,5];
+
+let multy = nums.forEach((num) => console.log(num * num))
+// output is 2,4,9,16,25
