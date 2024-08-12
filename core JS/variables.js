133c18ef36dbac20c2c68282a9aa54c3d0c289bd
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-12 23:21:48 +0530

    Create variables.js
    variables in JS

---
+++ core JS/variables.js
@@ -0,0 +1,21 @@
+// variables are declared automatically
+
+x = 10;
+console.log(x);
+
+//var keyword
+// avoid use it --- only use on older browsers
+var x = 15;
+console.log(x);
+
+//ES6 - 2015
+let a = 24;
+
+
+// always use const if we don't want to change value
+// always use const for declaring objects and arrays
+const b = 56;
+//b = 45; 
+
+a = 45;
+console.log("a",a);
