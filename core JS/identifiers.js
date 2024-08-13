c8627474c4314047814c7b0e0e1736c7fe3f96c4
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-13 23:07:17 +0530

    identifiers in JS

---
+++ core JS/identifiers.js
@@ -0,0 +1,35 @@
+let x = 10;
+console.log(x);
+
+const a = 13;
+console.log(a);
+
+const b = 12;
+console.log(b);
+
+const sum = a+b;
+console.log(sum);
+
+const Ajinkya$ = 12;
+console.log(Ajinkya$);
+
+const mul12 = 24;
+console.log(mul12);
+
+const mul_12 = 25;
+console.log(mul_12);
+
+
+// letters, numbers, underscore, doller
+
+// only start with letter, underscore, doller
+
+// cannot start with number
+
+const right23 = 45;
+
+const $age = 45;
+
+const _hobby = "swimming";
+
+//keywords = reserved words
