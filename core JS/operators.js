abd95fed34981f9d57686c61f98dd3107141ddbe
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-15 22:29:01 +0530

    basic's of operators in JS

---
+++ core JS/operators.js
@@ -0,0 +1,39 @@
+// types of operators
+
+/**
+ * 1.arthmatic operators
+ * 2.comparison operators
+ * 3.logical
+ * 4.assignment
+ * 5.conditional
+ * 6.type
+ */
+
+let a = 10;
+let b = 20;
+
+let add = a+b;
+console.log("a + b = ",add);
+
+// exponentiation
+let exp = a**b;
+console.log("exp = ",exp)
+
+let num = 1;
+
+num++; // post increment
+console.log("post increment num = ",num);
+
+++num; // pre increment
+console.log("pre increment num = ",num);
+
+let num1 = 1;
+
+num1--; // post increment
+console.log("post decrement num = ",num1);
+
+--num1; // pre increment
+console.log("pre decrement num = ",num1);
+
+
+
