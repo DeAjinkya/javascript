506115e5d017313ddc71e7e040425377dcdfebf1
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-16 23:26:58 +0530

    Assignment Operator in JS

---
+++ core JS/assignment.js
@@ -0,0 +1,49 @@
+// Assignment Operator
+
+let x = 10;
+let y = 20;
+
+x = x+y;
+console.log("x= ",x);
+
+x += y; // x = 30  y = 20
+console.log(x);
+
+x = x-y; // x = 50  y = 20
+console.log("x = ",x);
+
+x -= y; // x = 30  y = 20
+console.log(x);
+
+let a = 2;
+let b = 2;
+
+a *= b;
+console.log(a);
+
+a /= b; // a = 4  b = 2
+console.log(a);
+
+a %= b; // a = 2 b = 2   // or a = a%b
+console.log(a);
+
+let n = 5;
+
+n **= b; // 5 to the power 2
+console.log(n);
+
+let firstName = "ajinkya";
+let lastName = "deshmukh";
+
+firstName += lastName;
+
+console.log(firstName);
+
+const age = 20;
+const string = " is my age";
+
+const Statement = age + string;
+console.log(Statement);
+
+
+
