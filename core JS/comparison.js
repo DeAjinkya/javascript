6f19c7d44b1e26d195856628da3d57669c9a79d1
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-27 23:59:32 +0530

    Comparison operators in JS

---
+++ core JS/comparison.js
@@ -0,0 +1,23 @@
+// comparison operators
+
+let age = 20;
+
+const adult = age>=18;
+
+console.log(adult); // true
+
+let x = 10;
+let y = 10;
+
+const isXlessThanY = x<y;
+
+console.log(isXlessThanY); // false
+
+let num1 = 30;
+let num2 = 40;
+let num3 = "30";
+
+console.log(num1 == num2); // false
+
+console.log(num1 === num3); // false
+
