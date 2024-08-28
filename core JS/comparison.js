a5b390b5b2aaaff7e8486fbb669021f0eb9aa5ec
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2024-08-28 23:15:55 +0530

    comparison operators in JS

--- core JS/comparison.js
+++ core JS/comparison.js
@@ -1,5 +1,5 @@
 // comparison operators
-
+// <,>,<=,>=
 let age = 20;
 
 const adult = age>=18;
@@ -13,11 +13,29 @@ const isXlessThanY = x<y;
 
 console.log(isXlessThanY); // false
 
+// == and ===
 let num1 = 30;
 let num2 = 40;
-let num3 = "30";
+let num3 = "50";
+let num4 = 50;
 
-console.log(num1 == num2); // false
+console.log(num1 == num2); // false  
 
-console.log(num1 === num3); // false
+console.log(num3 == num4); // true  (it only compares values)
+
+console.log(num3 === num4); // false (it compares values and types of variables)
+
+//!= and !==
+let n1 = 56;
+let n2 = 89;
+let n3 = "30";
+let n4 = 30;
+
+console.log(n1 != n2); // true
+
+console.log(n1 !== n2); // true
+
+console.log(n3 != n4); // false
+
+console.log(n3 !== n4); // true (it compares values or compares types)
 
